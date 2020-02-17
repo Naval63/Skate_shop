a0df24bd0fd432e541442fbc31886f4717194a65
Bartosz Smalcerz <b.smalcerz@gmail.com>
2020-02-17 14:12:05 +0100

    add delete buton

--- src/js/script.js
+++ src/js/script.js
@@ -6,30 +6,49 @@ class Shop {
         this.sendButton.addEventLisner('click', this.submitItemData.bind(this))
     }
     async getShopData() {
-        const item = await this.getData();
+        const items = await this.getData();
         this.clearTableBody();
-        item.forEach((item) => {
-            const row = this.create.Row(item);
+        items.forEach((item) => {
+            const row = this.createRow(item);
             this.table.appendChild(row);
-        }); 
+        });
     }
-    getData = () => {
-        fetch(this.url).then(respone => response.json());
-    }
+    getData = () => fetch(this.url)
+        .then(response => response.json());
+
     createCell(value) {
         const td = document.createElement('td');
         td.innerHTML = value;
         return td;
     }
-    clearTableBody() {
-        this.table.innerHTML = '';
-    }
+   
     createRow(item) {
         const tr = document.createElement('tr');
         ['name', 'price', 'count'].forEach(key => {
             tr.appendChild(this.createCell(item.data[key]))
         });
+        tr.appendChild(this.deleteButton(item._id))
         return tr
+    }
+    clearTableBody() {
+        this.table.innerHTML = '';
+    }
+    deleteButton(id) {
+        const button = document.createElement('button');
+        button.innerText = 'Usuń';
+        button.dataset.itemId = id;
+        button.addEventListener('click', this.removeItemHandler.bind(this));
+        const cell = document.createElement('td');
+        cell.appendChild(button);
+        return cell;
+    }
+    removeItemHandler(e) {
+        const id = e.target.dataset.itemId;
+        const url = `${this.url}/${id}`;
+        fetch(url, {
+            method: 'DELETE'
+        })
+        .then(() => this.getShopData())
     }
     submitItemData() {
         const headers = new Headers();
@@ -46,7 +65,7 @@ class Shop {
             }
         if(request) {
             fetch(url, request)
-                .then(response => response.json)
+                .then(response => response.json())
                 .then(() => this.getShopData());
         }
     }
